40a85e5972b7a8024eab506a03d56906d107e7a1
Max <maxvangogh@home.nl>
2019-10-29 17:19:37 +0100

    Added initial values and started get classes

--- src/Model.hs
+++ src/Model.hs
@@ -20,14 +20,14 @@ type ShootBound = Float
 -- Damage multiplier of the player
 type Upgrades = Float
 
--- The direction the object is moving in
-type Direction = Vector
-
 -- The score of the player
 type Score = Int
 
 -- The amout of /seconds/ since the game began
 type Time = Float
+
+-- the possible movement directions
+data Direction = Left | Right | Up | Down
 
 -- The player
 data Player = Player { getHealth :: Health
@@ -46,14 +46,14 @@ data EnemyStats = Stats { getHealth :: Health
 -- The type of an enemy
 data EnemyType = Standard | Boss
 -- The constructor of the enemy
-data Enemy = Enemy {getType :: EnemyType, getStats :: EnemyStats}
+data Enemy = Enemy { getType :: EnemyType
+                   , getStats :: EnemyStats}
 
 -- The bullet
 data BulletType = EnemyBullet | PlayerBullet
 data Bullet = Bullet { getBulletType :: BulletType
                      , getLocation :: Location
-                     , getSpeed :: Speed
-                     , getDirection :: Direction
+                     , getVector :: Vector
                      , getDamage :: Damage
 }
 
@@ -69,11 +69,12 @@ data GameState = GameState { getScore :: Score
                            , getWorld :: World
 }
 
+-- A datastructure defining if an object is either living or dead
 data HealthState = Alive | Dead
 
---Type classes
+{- Type classes -}
 class Movable a where
-    move :: Vector -> a -> a 
+    move :: Vector -> a -> a
 
 class Renderable a where
     getPicture :: Picture
@@ -82,8 +83,7 @@ class Damageable a where
     damage :: a -> Damage -> a
     getHealthState :: a -> HealthState
 
--- Instances 
-
+{- Instances -}
 -- Movable instances    
 instance Movable Point where
     move v p = v + p
@@ -98,7 +98,7 @@ instance Movable Enemy where
     move v (Enemy t s) = Enemy t (move v s)
 
 instance Movable Bullet where
-    move v (Bullet bt l s dir d) = Bullet bt (move v l) s dir d
+    move v (Bullet bt l vec d) = Bullet bt (move v l) vec d
 
 -- Renderable instances
 instance Renderable World where
@@ -125,4 +125,58 @@ instance Damageable Enemy where
     damage (Enemy t s) dam     = Enemy t (damage s dam)
     getHealthState (Enemy t s) = getHealthState s
 
--- basic enemy types
+{- initial values for types -}
+-- initial player
+initialPlayer :: Player
+initialPlayer = Player h l s u
+  where
+    h :: Health
+    h = 10
+    l :: Location
+    l = (-150, 0)
+    s :: Speed
+    s = 1
+    u :: Upgrades
+    u = 0.0
+
+-- initial world
+initialWorld :: World
+initialWorld = World initialPlayer [] []
+
+-- initial gamestate
+initialGameState :: GameState
+initialGameState = GameState 0 0 initialWorld
+
+{- get object functions -}
+-- get a boss enemy at the specified location with specified health
+getBossEnemy :: Health -> Location -> Enemy
+getBossEnemy h l = Enemy t s
+  where
+    t :: EnemyType
+    t = Boss
+    s :: EnemyStats
+    s = Stats h l 1 40
+    
+-- get a standard enemy at the specified location with specified health
+getStandardEnemy :: Health -> Location -> Enemy
+getStandardEnemy h l = Enemy t s
+  where
+    t :: EnemyType
+    t = Standard
+    s :: EnemyStats
+    s = Stats h l 2 20
+
+-- get a player bullet, moving right
+getPlayerBullet :: Location -> Damage -> Bullet
+getPlayerBullet l d = Bullet PlayerBullet l (getVector Right 3) d
+
+-- get an enemy bullet, moving left
+getEnemyBullet :: Location -> Damage -> Bullet
+getEnemyBullet l d = Bullet EnemyBullet l (getVector Left 3) d
+
+-- get a vector, given a direction and speed
+getVector :: Direction -> Speed -> Vector
+getVector Left s  = (-s, 0)
+getVector Right s = (s, 0)
+getVector Up s    = (0, -s)
+getVector Down s  = (0, s)
